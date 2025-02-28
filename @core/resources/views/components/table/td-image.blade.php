2d7bc31e2d7c12805a231340166590fcb55a4299
james-software0312 <james.cameron9075@gmail.com>
2025-02-28 13:38:10 +0900

    products-imgae-update

--- @core/resources/views/components/table/td-image.blade.php
+++ @core/resources/views/components/table/td-image.blade.php
@@ -1,5 +1,5 @@
 <td>
-    <div class="img-wrap max-width-100">
+    <div class="img-wrap max-width-100" style="width: 50px!important;">
         {!! render_image_markup_by_attachment_id($image, '', 'grid') !!}
     </div>
 </td>
